cb6804a556cf5c9df49975d2c10b2fa75e1faae1
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-05-15 14:21:36 +0900

    デバッグ情報削除

--- message-editor-util.php
+++ message-editor-util.php
@@ -13,10 +13,6 @@ class message_editor_util {
 
         $followed = qa_db_read_one_value(qa_db_query_sub($sql, $touserid, $loginuserid));
 
-        var_export($loginuserid);
-        var_export($touserid);
-        var_export($following);
-        var_export($followed);
         return $following && $followed;
     }
 }
