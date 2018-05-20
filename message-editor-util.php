bb2d15670b8b1daf5c06c12726848db295e17b10
yshiga <yuichi.shiga@gmail.com>
2018-05-21 00:51:19 +0900

    bug fix

--- message-editor-util.php
+++ message-editor-util.php
@@ -26,18 +26,31 @@ class message_editor_util {
      * 相互フォローしていないと利用できない
      */
     public static function allow_message($loginuserid, $toaccount)
-    {
+    { 
+        // サイト設定でOFFの場合や送信先がおかしい場合はfalse返す(UIや文言との不整合が起こる)
+        if(!qa_opt('allow_private_messages') || !is_array($toaccount)) {
+            return false;
+        }
+
+        // 管理人は無条件OK 
         if (qa_get_logged_in_level() >= QA_USER_LEVEL_ADMIN
             || $toaccount['level'] >= QA_USER_LEVEL_ADMIN) {
             return true;
         }
-        if ( !qa_opt('allow_private_messages') || !is_array($toaccount)
-          || ((!($toaccount['flags'] & !QA_USER_FLAGS_NO_MESSAGES)
-          || !(qa_get_logged_in_flags() & !QA_USER_FLAGS_NO_MESSAGES))
-          && !self::follow_each_other($loginuserid, $toaccount['userid'])) ) {
-            return false;
-        } else {
+        // 相互フォローも無条件OK
+        if(self::follow_each_other($loginuserid, $toaccount['userid'])) {
+             return true;
+        }
+
+        $to_ok = !($toaccount['flags'] & QA_USER_FLAGS_NO_MESSAGES);
+        $me_ok = !(qa_get_logged_in_flags() & QA_USER_FLAGS_NO_MESSAGES);
+
+        // 両方ともOkなら、送信可能
+        if($to_ok && $me_ok) {
             return true;
         }
+
+        return false;
     }
 }
+
